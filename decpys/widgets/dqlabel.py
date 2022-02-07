a4edda9ce3cf7d8062ed818c5b98a835a5906c50
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-07 18:00:55 -0500

    define set_text()

--- decpys/widgets/dqlabel.py
+++ decpys/widgets/dqlabel.py
@@ -2,11 +2,31 @@
 """
 
 
+from __future__ import annotations
 from PySide6.QtWidgets import QLabel
 
 
 class DQLabel(QLabel):
 
 
-    def __init__(self):
-        super()
+    def __init__(self, text: str = None):
+        """ Returns a QLabel wrapper for displaying text, movies, images, etc.
+        """
+        if text:
+            super(text)
+        else:
+            super()
+
+
+    def set_alignment(self, alignment: int) -> DQLabel:
+        """ Sets this label's alignment using alignment flags from PySide6.
+        """
+        self.setAlignment(alignment)
+        return self
+
+
+    def set_text(self, text: str) -> DQLabel:
+        """ Sets this label's text.
+        """
+        self.setText(text)
+        return self
