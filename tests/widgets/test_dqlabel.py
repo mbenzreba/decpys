14e1bb99c4489f4cbb5f3b8617afa568298468e7
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-06 20:32:57 -0500

    test that DQLabel registers as an instance of QLabel

--- tests/widgets/test_dqlabel.py
+++ tests/widgets/test_dqlabel.py
@@ -3,8 +3,10 @@
 
 
 import define_pkg_path
+from PySide6.QtWidgets import QLabel
 from decpys.widgets import DQLabel
 
 
 def test_dqlabel():
     assert isinstance(DQLabel(), DQLabel)
+    assert isinstance(DQLabel(), QLabel)
