6e07d48cf8205c6a0e86efd80dd100c47ac28b23
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-08 16:24:07 -0500

    redefine getters and setters to use same naming scheme as original QLabel

--- decpys/widgets/dqlabel.py
+++ decpys/widgets/dqlabel.py
@@ -2,31 +2,53 @@
 """
 
 
+
+# built-in
 from __future__ import annotations
-from PySide6.QtWidgets import QLabel
+from typing import Tuple
+
+# base
+from PySide6.QtWidgets import QLabel, QWidget
+from PySide6.QtCore import Qt
+
+# decpys
+from decpys.core import AlignmentFlag
+
 
 
 class DQLabel(QLabel):
+    """ Used to show text, movies, pictures. etc. as a label.
+    """
+
 
 
     def __init__(self, text: str = None):
         """ Returns a QLabel wrapper for displaying text, movies, images, etc.
         """
-        if text:
-            super(text)
-        else:
-            super()
+        super().__init__(text)
 
 
-    def set_alignment(self, alignment: int) -> DQLabel:
+
+    def alignment(self) -> Tuple[AlignmentFlag, AlignmentFlag]:
+        """ Returns the alignment of this widget contained in a 2-element tuple, where the first 
+        element of the tuple is the horizontal alignment and the second is the vertical alignment.
+        """
+        return (AlignmentFlag.from_int(super().alignment() & Qt.AlignHorizontal_Mask), 
+                AlignmentFlag.from_int(super().alignment() & Qt.AlignVertical_Mask))
+
+
+
+    def setAlignment(self, alignment: AlignmentFlag) -> DQLabel:
         """ Sets this label's alignment using alignment flags from PySide6.
         """
-        self.setAlignment(alignment)
+        super().setAlignment(Qt.AlignmentFlag(alignment.value))
         return self
 
 
-    def set_text(self, text: str) -> DQLabel:
+
+    def setText(self, text: str) -> DQLabel:
         """ Sets this label's text.
         """
-        self.setText(text)
+        super().setText(text)
         return self
+    
