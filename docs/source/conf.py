01c2c1b1b4a913565245a791d8e87d81a8fb2402
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-07 19:39:29 -0500

    style html output

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
+
+html_theme_options = {
+    "footerbgcolor": "#554640",
+    "sidebarbgcolor": "#707078",
+    "sidebartextcolor": "#8da7be",
+    "sidebarlinkcolor": "#cde6f5",
+    "relbarbgcolor": "#554640"
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
