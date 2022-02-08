5cfb5be0a397abb10c392c41ab5e25e7fc852b2b
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-08 16:27:41 -0500

    test alignment

--- tests/widgets/test_dqlabel.py
+++ tests/widgets/test_dqlabel.py
@@ -2,11 +2,54 @@
 """
 
 
+
+# built-in
+import pytest
 import define_pkg_path
+
+# pyside
 from PySide6.QtWidgets import QLabel
+from PySide6.QtWidgets import QApplication
+
+# decpys
 from decpys.widgets import DQLabel
+from decpys.core import AlignmentFlag
 
 
-def test_dqlabel():
+
+@pytest.fixture
+def init_qapp() -> QApplication:
+    """ A QApplication must exist before any QWidget is instantiated.
+    """
+    return QApplication()
+
+
+
+def test_dqlabel(init_qapp):
+    """ Test that a DQLabel can be instantiated and is type DQLabel.
+    """
+    app = init_qapp
     assert isinstance(DQLabel(), DQLabel)
+    
+
+
+def test_dqlabel_is_qlabel():
+    """ Test that a DQLabel is an instance of QLabel.
+    """
+    app = init_qapp
     assert isinstance(DQLabel(), QLabel)
+
+
+
+def test_dqlabel_alignment():
+    """ Test that a DQLabel's alignment can be set and get.
+
+    BUG: For some reason, the label's horizontal alignment always returns 0 here. I don't
+    think it can be set in this kind of environment.
+    TODO: Recheck the appearance of this alignment bug in E2E testing. 
+    """
+    app = init_qapp
+    label = DQLabel("label1").setAlignment(AlignmentFlag.ALIGN_VCENTER)
+    assert label.alignment() == (AlignmentFlag.ALIGN_NONE, AlignmentFlag.ALIGN_VCENTER, )
+    label = label.setAlignment(AlignmentFlag.ALIGN_TOP)
+    assert label.alignment() == (AlignmentFlag.ALIGN_NONE, AlignmentFlag.ALIGN_TOP, )
