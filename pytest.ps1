f07a724fb119a6ceb510ae414c65aa6957cbeb01
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-11 20:02:46 -0500

    run pytests seperately due to QGuiApplication singleton issue

---
+++ pytest.ps1
@@ -0,0 +1,8 @@
+# This script is needed to run pytests due to QApplication singleton bug.
+# The QApplication cannot be torn down effectively after each test module is run in pytest, so a
+# RuntimeError is thrown as soon as the second script is reached and attempts instantiation of a
+# new application.
+
+pytest tests/core/test_qt.py
+pytest tests/gui/test_qicon.py
+pytest tests/widgets/test_qlabel.py
