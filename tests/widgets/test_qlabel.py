788076d3ba012e78f3d4d8528a936de5f0150ec6
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-11 19:58:49 -0500

    use init_qapp fixture in test method args

--- tests/widgets/test_qlabel.py
+++ tests/widgets/test_qlabel.py
@@ -10,6 +10,7 @@ import define_pkg_path
 # pyside
 from PySide6.QtWidgets import QLabel as BaseQLabel
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QGuiApplication
 
 # decpys
 from decpys.widgets import QLabel
@@ -17,38 +18,33 @@ from decpys.core import Alignment
 
 
 
-@pytest.fixture
-def init_qapp() -> QApplication:
-    """ A QApplication must exist before any QWidget is instantiated.
-    """
-    return QApplication()
-
+@pytest.fixture(scope="module")
+def init_qapp():
+    app = QApplication()
+    yield app
 
 
 def test_qlabel(init_qapp):
     """ Test that a QLabel can be instantiated and is type QLabel.
     """
-    app = init_qapp
     assert isinstance(QLabel(), QLabel)
     
 
 
-def test_qlabel_is_qlabel():
+def test_qlabel_is_qlabel(init_qapp):
     """ Test that a QLabel is an instance of QLabel.
     """
-    app = init_qapp
     assert isinstance(QLabel(), BaseQLabel)
 
 
 
-def test_qlabel_alignment():
+def test_qlabel_alignment(init_qapp):
     """ Test that a QLabel's alignment can be set and get.
 
     BUG: For some reason, the label's horizontal alignment always returns 0 here. I don't
     think it can be set in this kind of environment.
     TODO: Recheck the appearance of this alignment bug in E2E testing. 
     """
-    app = init_qapp
     label = QLabel("label1").setAlignment(Alignment.ALIGN_VCENTER)
     assert label.alignment() == (Alignment.ALIGN_NONE, Alignment.ALIGN_VCENTER, )
     label = label.setAlignment(Alignment.ALIGN_TOP)
