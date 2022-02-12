6b3d9a621879e1d1cb619e407fd233fed6c1dd4e
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-11 20:00:54 -0500

    define local package path for decpys

---
+++ tests/gui/define_pkg_path.py
@@ -0,0 +1,16 @@
+""" Import names needed by tests run in tests/gui.
+"""
+
+
+import sys, os
+
+
+decpys_pkg = os.path.join("..", "decpys")
+decpys_core_pkg = os.path.join("..", "decpys", "core")
+decpys_gui_pkg = os.path.join("..", "decpys", "gui")
+decpys_widgets_pkg = os.path.join("..", "decpys", "widgets")
+
+sys.path.append(decpys_pkg)
+sys.path.append(decpys_core_pkg)
+sys.path.append(decpys_gui_pkg)
+sys.path.append(decpys_widgets_pkg)
