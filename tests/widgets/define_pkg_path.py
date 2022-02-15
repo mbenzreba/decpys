754f95e19c594581f6db7cf889bb250e6e26db8e
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-15 14:54:02 -0500

    add path to decpys.gui subpackage

--- tests/widgets/define_pkg_path.py
+++ tests/widgets/define_pkg_path.py
@@ -7,8 +7,10 @@ import sys, os
 
 decpys_pkg = os.path.join("..", "decpys")
 decpys_core_pkg = os.path.join("..", "decpys", "core")
+decpys_gui_pkg = os.path.join("..", "decpys", "gui")
 decpys_widgets_pkg = os.path.join("..", "decpys", "widgets")
 
 sys.path.append(decpys_pkg)
 sys.path.append(decpys_core_pkg)
+sys.path.append(decpys_gui_pkg)
 sys.path.append(decpys_widgets_pkg)
