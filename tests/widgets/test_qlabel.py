ed472998c60e2a0d190c94479c1115ebfb5a1211
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-15 20:35:46 -0500

    test qLabel()

--- tests/widgets/test_qlabel.py
+++ tests/widgets/test_qlabel.py
@@ -12,44 +12,50 @@ from PySide6.QtWidgets import QLabel as BaseQLabel
 from PySide6.QtWidgets import QApplication
 
 # decpys
-from decpys.widgets import QLabel
+from decpys.widgets import QLabel, qLabel
 from decpys.core import Alignment
 
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def init_qapp() -> QApplication:
     """ A QApplication must exist before any QWidget is instantiated.
     """
-    return QApplication()
+    app = QApplication()
+    yield app
 
 
 
 def test_qlabel(init_qapp):
     """ Test that a QLabel can be instantiated and is type QLabel.
     """
-    app = init_qapp
     assert isinstance(QLabel(), QLabel)
     
 
 
-def test_qlabel_is_qlabel():
+def test_qlabel_is_qlabel(init_qapp):
     """ Test that a QLabel is an instance of QLabel.
     """
-    app = init_qapp
     assert isinstance(QLabel(), BaseQLabel)
 
 
 
-def test_qlabel_alignment():
+def test_qlabel_alignment(init_qapp):
     """ Test that a QLabel's alignment can be set and get.
 
     BUG: For some reason, the label's horizontal alignment always returns 0 here. I don't
     think it can be set in this kind of environment.
     TODO: Recheck the appearance of this alignment bug in E2E testing. 
     """
-    app = init_qapp
     label = QLabel("label1").setAlignment(Alignment.ALIGN_VCENTER)
     assert label.alignment() == (Alignment.ALIGN_NONE, Alignment.ALIGN_VCENTER, )
     label = label.setAlignment(Alignment.ALIGN_TOP)
     assert label.alignment() == (Alignment.ALIGN_NONE, Alignment.ALIGN_TOP, )
+
+
+def test_qlabel_function(init_qapp):
+    """ Assert that qLabel() can be used in a declarative way to construct a QLabel object.
+    """
+    label = qLabel(text="original").setText("modified")
+    assert label.text() == "modified", \
+            "qLabel() must provide a QLabel object that can be modified in the same line as it is called."
