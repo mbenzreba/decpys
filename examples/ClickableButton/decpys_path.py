7ed5ad277ebc0ca12a8c4152f5f722a76d56e56c
Mohamed Benzreba <mbenzreba05@gmail.com>
2022-02-20 22:47:48 -0500

    add local decpys package path to system path for python interpreter

---
+++ examples/ClickableButton/decpys_path.py
@@ -0,0 +1,20 @@
+"""
+"""
+
+
+# built-in
+import sys, os
+
+
+
+#####################
+#       SCRIPT      #
+#####################
+
+
+
+# paths to decpys package
+decpys_pkg = os.path.join("..", "decpys")
+
+# append paths to system environment
+sys.path.append(decpys_pkg)
